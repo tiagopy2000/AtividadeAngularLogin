ca379b19bf964f976a806ce17c61ad9c657aeea6
tiagopy2000 <tiagosocial2001@gmail.com>
2022-09-11 18:14:02 -0300

    Codigo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { LoginComponent } from './views/login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './views/contato/contato.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProdutosComponent } from './views/produtos/produtos.component'
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent
+  },
+
+  {
+    path: 'produtos',
+    component: ProdutosComponent
+  },
+
+  
+  {
+    path: 'contato',
+    component: ContatoComponent 
+  },
+  
+  {
+    path: 'login',
+    component: LoginComponent 
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+
+///componetes
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+//paginas
+import { HomeComponent } from './views/home/home.component';
+import { ProdutosComponent } from './views/produtos/produtos.component';
+import { ContatoComponent } from './views/contato/contato.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './views/login/login.component';
+import { AuthService } from './views/login/auth.service';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProdutosComponent,
+    ContatoComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
